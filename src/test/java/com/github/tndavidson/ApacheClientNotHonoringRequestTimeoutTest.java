4ac964fafa3d39f6be2d22a1fd27e25fdb20c57c
pauleftis-vaprofile <paul.eftis@va.gov>
2024-04-16 00:51:37 -0400

    quick updates and little refactoring

--- src/test/java/com/github/tndavidson/ApacheClientNotHonoringRequestTimeoutTest.java
+++ src/test/java/com/github/tndavidson/ApacheClientNotHonoringRequestTimeoutTest.java
@@ -5,12 +5,10 @@ import static com.github.tomakehurst.wiremock.client.WireMock.get;
 
 import java.io.IOException;
 import java.lang.Thread.State;
-import java.net.SocketTimeoutException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -36,9 +34,9 @@ import org.apache.hc.core5.http.io.HttpClientResponseHandler;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.nio.ssl.TlsStrategy;
 import org.apache.hc.core5.io.CloseMode;
-import org.apache.hc.core5.reactor.IOReactorStatus;
 import org.apache.hc.core5.ssl.SSLContexts;
 import org.apache.hc.core5.ssl.TrustStrategy;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -52,13 +50,19 @@ import com.github.tomakehurst.wiremock.junit.WireMockRule;
 public class ApacheClientNotHonoringRequestTimeoutTest {
 	
     private static final Logger LOG = LoggerFactory.getLogger(ApacheClientNotHonoringRequestTimeoutTest.class);
-
+    
+    // forceImmediateClose false means use graceful close
+    private static boolean forceImmediateClose = true;
+    
+    // for sync https client
     private static final SimpleResponseHandler handler = new SimpleResponseHandler();
     
-    // threadList is just used to get references to callable threads in executor service pool
-    // to display their runtime status
-    private static final List<Thread> threadList = new ArrayList<>();
+    // for async https client setup
+    private static TrustStrategy acceptingTrustStrategy;
+    private static SSLContext sslContext;
+    private static TlsStrategy tlsStrategy;
 
+    
     @Rule
     public WireMockRule wireMockRule = new WireMockRule(WireMockConfiguration.wireMockConfig().httpsPort(8443)
             .keystorePath("./src/test/resources/server-keystore.jks")
@@ -67,6 +71,26 @@ public class ApacheClientNotHonoringRequestTimeoutTest {
             .trustStorePath("./src/test/resources/server-truststore.jks")
             .trustStorePassword("secret")
             .httpDisabled(true));
+    
+    // threadList is just used to get references to callable threads in executor service pool
+    // to display their runtime status
+    private static final List<Thread> threadList = new ArrayList<>();
+    
+    @Before
+    public void setupClientTls() {
+    	try {
+        	acceptingTrustStrategy = (certificate, authType) -> true;
+            sslContext = SSLContexts.custom()
+                .loadTrustMaterial(null, acceptingTrustStrategy)
+                .build();
+            tlsStrategy = ClientTlsStrategyBuilder.create()
+                .setHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+                .setSslContext(sslContext)
+                .build();
+    	} catch(Exception e) {
+    		LOG.error("Exception setting up tls stuff: ", e);
+    	}
+    }
 
     /**
      * This test reproduces the issue I am running into in my application. You can see by looking at the logs,
@@ -102,7 +126,8 @@ public class ApacheClientNotHonoringRequestTimeoutTest {
 	            	LOG.debug("Timeout Fired and No Response Yet");
 	                aggregatedResponse.add("No Response From Zombie Partner");
 	                // you can do f.cancel(true) or get reference to thread (using threadList)
-	                // and do thread.interupt() but nothing works to kill thread blocked in ssl io (been there tried it)
+	                // and do thread.interupt() but nothing works to kill the underlying thread 
+	                // that is blocked in ssl io (been there tried it)
 	            } else {
 	            	try {
 		            	LOG.debug("Future completed and returned : " + f.get());
@@ -135,13 +160,13 @@ public class ApacheClientNotHonoringRequestTimeoutTest {
                 get(WireMock.urlMatching("/test-tls-zombie")).willReturn(aResponse().withFixedDelay(20000)));
 		List<String> aggregatedResponse = new ArrayList<>();
         
-        List<Future<String>> futureList = new ArrayList<>();
         long startTime = System.currentTimeMillis();
 		try {
 			LOG.debug("Executor Invocation Start");
 			// Timeout of 5 seconds
-			futureList = executor.invokeAll(Arrays.asList(new AsyncHttpsCallable(handler), new AsyncHttpsCallable(handler), 
-					new AsyncHttpsHangingCallable(handler)), 5000L, TimeUnit.MILLISECONDS);
+			List<Future<String>> futureList = executor.invokeAll(Arrays.asList(new AsyncHttpsCallable("https://localhost:8443/test-tls"), 
+					new AsyncHttpsCallable("https://localhost:8443/test-tls"), 
+					new AsyncHttpsHangingCallable("https://localhost:8443/test-tls-zombie")), 5000L, TimeUnit.MILLISECONDS);
 			LOG.debug("Executor Invocation Completed");
 			try {
 		        for(Future<String> f : futureList) {
@@ -165,7 +190,12 @@ public class ApacheClientNotHonoringRequestTimeoutTest {
 
         // sleep/wait to continue getting logs and thread status
         monitorThreadPoolThreadsTillCleared();
-        System.out.println("*******  total time for all threads to clear (millis): " + Long.toString(System.currentTimeMillis() - startTime) + "  ******");
+        if(forceImmediateClose) {
+        	System.out.println("*******  using forceImmediateClose=true total time for all threads to be force cleared (millis): " + Long.toString(System.currentTimeMillis() - startTime) + "  ******");
+        } else {
+        	System.out.println("*******  using forceImmediateClose=false total time for all threads to gracefully clear (millis): " + Long.toString(System.currentTimeMillis() - startTime) + "  ******");
+        }
+        
         finalDumpAndClearThreadList();
         executor.shutdown();
     }
@@ -226,98 +256,100 @@ public class ApacheClientNotHonoringRequestTimeoutTest {
     }
 
     
+
+    /**
+     * CloseableHttpAsyncClient has the wonderful feature that you can
+     * call CloseableHttpAsyncClient.close(CloseMode) which will immediately
+     * interrupt the https io and either gracefully close or immediately close depending
+     * on the CloseMode used.  By breaking the thread out of the ssl io it is blocking on,
+     * the thread is allowed to end/complete and thus return to the pool.
+     * So just make sure call close in finally, and hanging thread issue due to zombie partner just goes away ;)
+     */
     static class AsyncHttpsHangingCallable implements Callable<String>
     {
-    	SimpleResponseHandler handler;
-    	public AsyncHttpsHangingCallable(SimpleResponseHandler h) {
-    		handler = h;
+    	private final String url;
+    	public AsyncHttpsHangingCallable(String endpoint) {
+    		url = endpoint;
     	}
     	
         public String call() throws Exception {
         	threadList.add(Thread.currentThread());
         	String responseStr = null;
         	
-            TrustStrategy acceptingTrustStrategy = (certificate, authType) -> true;
-
-            SSLContext sslContext = SSLContexts.custom()
-                .loadTrustMaterial(null, acceptingTrustStrategy)
-                .build();
-
-            TlsStrategy tlsStrategy = ClientTlsStrategyBuilder.create()
-                .setHostnameVerifier(NoopHostnameVerifier.INSTANCE)
-                .setSslContext(sslContext)
-                .build();
-
-            PoolingAsyncClientConnectionManager cm = PoolingAsyncClientConnectionManagerBuilder.create()
-                .setTlsStrategy(tlsStrategy)
-                .build();
-
+        	PoolingAsyncClientConnectionManager asyncConnectionManager = PoolingAsyncClientConnectionManagerBuilder.create()
+                    .setTlsStrategy(tlsStrategy)
+                    .build();
             CloseableHttpAsyncClient client = HttpAsyncClients.custom()
-                .setConnectionManager(cm)
+                .setConnectionManager(asyncConnectionManager)
                 .build();
-
             client.start();
-
-            SimpleHttpRequest request = new SimpleHttpRequest("GET", "https://localhost:8443/test-tls-zombie");
-            System.out.println("******* async client calling https://localhost:8443/test-tls-zombie ******");
+            SimpleHttpRequest request = new SimpleHttpRequest("GET", url);
+            System.out.println("******* async client calling " + url + " ******");
             Future<SimpleHttpResponse> future = client.execute(request, null);
 
             LOG.debug("GET request execute returned with future");
+            boolean wasInterrupted = false;
             try {
 	            SimpleHttpResponse response = future.get();
 	            // will never get here as blocking/waiting for response that never comes
+	            // fortunately, async future get will break and throw out InterruptedException 
+	            // if callable cancelled
 	            LOG.debug("future get() returned");
 	            responseStr = response.getBodyText();
-            } catch(Exception e) {
+            } catch(InterruptedException e) {
             	// catch the interrupted exception that is thrown so that
-            	// and ensure we close the client
+            	// can flag case when callable wasInterrupted due to timeout
             	LOG.error("Async Client was interrupted");
+            	wasInterrupted = true;
             } finally {
 	            LOG.debug("closing client");
-	            client.close();
+	            client.close(wasInterrupted && forceImmediateClose ? CloseMode.IMMEDIATE : CloseMode.GRACEFUL);
             }
             return responseStr;
         }
     }
     
+    /**
+     * CloseableHttpAsyncClient has the wonderful feature that you can
+     * call CloseableHttpAsyncClient.close(CloseMode) which will immediately
+     * interrupt the https io and either gracefully close or immediately close depending
+     * on the CloseMode used.  By breaking the thread out of the ssl io it is blocking on,
+     * the thread is allowed to end/complete and thus return to the pool.
+     * So just make sure call close in finally, and hanging thread issue due to zombie partner just goes away ;)
+     */
     static class AsyncHttpsCallable implements Callable<String>
     {
-    	SimpleResponseHandler handler;
-    	public AsyncHttpsCallable(SimpleResponseHandler h) {
-    		handler = h;
+    	private final String url;
+    	public AsyncHttpsCallable(String endpoint) {
+    		url = endpoint;
     	}
     	
         public String call() throws Exception {
         	threadList.add(Thread.currentThread());
         	String responseStr = null;
-        	
-            TrustStrategy acceptingTrustStrategy = (certificate, authType) -> true;
-            SSLContext sslContext = SSLContexts.custom()
-                .loadTrustMaterial(null, acceptingTrustStrategy)
-                .build();
-            TlsStrategy tlsStrategy = ClientTlsStrategyBuilder.create()
-                .setHostnameVerifier(NoopHostnameVerifier.INSTANCE)
-                .setSslContext(sslContext)
-                .build();
-            PoolingAsyncClientConnectionManager cm = PoolingAsyncClientConnectionManagerBuilder.create()
-                .setTlsStrategy(tlsStrategy)
-                .build();
+
+        	PoolingAsyncClientConnectionManager asyncConnectionManager = PoolingAsyncClientConnectionManagerBuilder.create()
+                    .setTlsStrategy(tlsStrategy)
+                    .build();
             CloseableHttpAsyncClient client = HttpAsyncClients.custom()
-                .setConnectionManager(cm)
+                .setConnectionManager(asyncConnectionManager)
                 .build();
             client.start();
-
-            SimpleHttpRequest request = new SimpleHttpRequest("GET", "https://localhost:8443/test-tls");
-            System.out.println("******* async client calling https://localhost:8443/test-tls ******");
+            SimpleHttpRequest request = new SimpleHttpRequest("GET", url);
+            System.out.println("******* async client calling " + url + " ******");
             Future<SimpleHttpResponse> future = client.execute(request, null);
+            boolean wasInterrupted = false;
             try {
 	            SimpleHttpResponse response = future.get();
 	            responseStr = response.getBodyText();
-            }catch(Exception e) {
-            	LOG.error("Async Client exception: ", e);
+            } catch(InterruptedException e) {
+            	// catch the interrupted exception that is thrown so that
+            	// can flag case when callable was cancelled due to timeout
+            	LOG.error("Async Client was interrupted");
+            	wasInterrupted = true;
             } finally {
 	            LOG.debug("closing client");
-	            client.close();
+	            client.close(wasInterrupted && forceImmediateClose ? CloseMode.IMMEDIATE : CloseMode.GRACEFUL);
             }
             return responseStr;
         }
